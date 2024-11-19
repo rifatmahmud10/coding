8ea5beb07480207d56336eef371461e4a170061c
rifat <rifatmahmud619@gmail.com>
2024-11-20 01:45:52 +0600

    solving majority element using moore voting

---
+++ python/majority_element_moore_voting.py
@@ -0,0 +1,15 @@
+def majority_element(nums):
+    count = 0
+    candidate = 0
+
+    for num in nums:
+        if count == 0:
+            candidate = num
+        if num == candidate:
+            count += 1
+        else:
+            count -= 1
+    return candidate
+
+
+print(majority_element([3, 3, 3, 2, 4, 2]))
